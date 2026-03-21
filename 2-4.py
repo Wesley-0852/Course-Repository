2c4ee8b81ba3e8f3e7e12e892f4771c1ba466732
Wesley <wesleyvosza@outlook.com>
2026-03-21 10:24:09 +0200

    Lecture 2: variables and data types

---
+++ 2-4.py
@@ -0,0 +1,15 @@
+print(8 * 60)
+
+hours = 8
+minutes_per_hour = 60
+minutes = hours * minutes_per_hour
+print(minutes)
+
+hours = 10
+minutes = hours * minutes_per_hour
+print(minutes)
+
+hours = 10.5
+minutes = hours * minutes_per_hour
+print(minutes)
+
---
+++ Lesson2/2-1.py
@@ -0,0 +1,12 @@
+age = 21
+print(age)
+
+age = 22
+print(age)
+
+name = "Wesley"
+print(name)
+
+is_student - True
+print(is_student)
+
